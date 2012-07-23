e735e7fe527d2c6cfb9c80270a4e540cadbb9448
shkh <kohei.accnt@gmail.com>
2012-07-23 15:11:29 +0900

    エスケープの修正

--- exercise1-1/lib/TemplateEngine.pm
+++ exercise1-1/lib/TemplateEngine.pm
@@ -7,7 +7,7 @@ use Encode;
 
 sub new{
     my ($self, %hsh) = @_;
-    
+
     open my $template, "$hsh{file}"
         or die "ファイル開けない $!\n";
 
@@ -19,14 +19,26 @@ sub render{
     
     my $lines = join '', <$self>;
     
+    my %esc_hsh = (
+        '&' => '&amp;', 
+        '>' => '&gt;', 
+        '<' => '&lt;', 
+        '"' => '&quot;', 
+    );
+    
     for my $key (keys %$hsh_ref){
         my $value = $$hsh_ref{$key};
-        $value =~ s/&/&amp;/g;
-        $value =~ s/>/&gt;/g;
-        $value =~ s/</&lt;/g;
-        $value =~ s/"/&quot;/g;
+
+        for my $esc_key (keys %esc_hsh){
+            $value =~ s/${esc_key}/$esc_hsh{$esc_key}/g;
+        }
+
+        #$value =~ s/&/&amp;/g;
+        #$value =~ s/>/&gt;/g;
+        #$value =~ s/</&lt;/g;
+        #$value =~ s/"/&quot;/g;
     
-        $lines =~ s/{%\s+$key\s+%}/$value/g;
+        $lines =~ s/{\s*%\s+$key\s+%\s*}/$value/g;
     }
     
     return encode('utf8', $lines); 
